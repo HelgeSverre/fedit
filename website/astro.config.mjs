0f4ca5d2de2515821b7d4b552a81608d35f10827
Helge Sverre <helge.sverre@gmail.com>
2026-05-28 23:18:26 +0200

    wip

--- website/astro.config.mjs
+++ website/astro.config.mjs
@@ -1,10 +1,12 @@
 import { defineConfig } from "astro/config";
 import sitemap from "@astrojs/sitemap";
 
+import inspectClip from "astro-inspect-clip";
+
 // https://astro.build/config
 export default defineConfig({
   site: "https://fedit.dev",
-  integrations: [sitemap()],
+  integrations: [sitemap(), inspectClip()],
   build: {
     inlineStylesheets: "auto",
   },
